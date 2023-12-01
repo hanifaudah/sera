85b0806712de10f9a15f7002456f8fa8e4756da6
hanifaudah <hanifarkanaudah@gmail.com>
2023-12-01 22:52:13 +0700

    test(api): update unit tests

--- api/tests/unit/emailTemplateCrudService.test.ts
+++ api/tests/unit/emailTemplateCrudService.test.ts
@@ -27,39 +27,41 @@ const beforeFunction = function () {
 describe('testing validateSlug service', function () {
   before(beforeFunction)
 
-  return (async () => {
-    it('should not throw error', async () => {
+  it('should not throw error', async () => {
+    return (async () => {
       assert.doesNotThrow(async () => await validateSlug('a-b'))
-    })
+    })()
+  })
 
-    it('should throw error', async () => {
+  it('should throw error', async () => {
+    return (async () => {
       const error = new StandardError('Invalid slug format, follow this pattern: /^[a-z0-9-]+[a-z0-9]+$/', { status: 400 })
       assert.rejects(async () => await validateSlug('a b'), error as Error)
-    })
-  })()
+    })()
+  })
 })
 
 describe('testing listEmailTemplates service', function () {
   before(beforeFunction)
 
-  return (async () => {
-    it('should return array', async () => {
+  it('should return array', async () => {
+    return (async () => {
       const emailTemplates = await listEmailTemplates()
       assert.equal(Array.isArray(emailTemplates), true)
-    })
-  })()
+    })()
+  })
 })
 
 describe('testing createEmailTemplate service', function () {
   before(beforeFunction)
 
-  return (async () => {
-    await createEmailTemplate({
-      slug: 'a-1',
-      template: 'hello'
-    })
-    const error = new StandardError('Slug already exists', { status: 400 })
-    it('should throw error', async () => {
+  const error = new StandardError('Slug already exists', { status: 400 })
+  it('should throw error', async () => {
+    return (async () => {
+      await createEmailTemplate({
+        slug: 'a-1',
+        template: 'hello'
+      })
       assert.rejects(
         async () => await createEmailTemplate({
           slug: 'a-1',
@@ -67,57 +69,62 @@ describe('testing createEmailTemplate service', function () {
         }),
         error as Error
       )
-    })
-  })()
+    })()
+  })
 })
 
 describe('testing retrieveEmailTemplate service', function () {
   before(beforeFunction)
 
-  return (async () => {
-    const existingEmail = await createEmailTemplate({
-      slug: 'a-1',
-      template: 'hello'
-    })
-    const error = new StandardError('Slug already exists', { status: 400 })
-    it('should not throw error', async () => {
+  it('should not throw error', async () => {
+    return (async () => {
+      const existingEmail = await createEmailTemplate({
+        slug: 'a-1',
+        template: 'hello'
+      })
       assert.doesNotThrow(async () => await retrieveEmailTemplate(existingEmail.id))
-    })
-  })()
+    })()
+  })
 })
 
 describe('testing deleteEmailTemplate service', function () {
   before(beforeFunction)
 
-  return (async () => {
-    const existingEmail = await createEmailTemplate({
-      slug: 'a-1',
-      template: 'hello'
-    })
-    it('should not throw error', async () => {
+  it('should not throw error', async () => {
+    return (async () => {
+      const existingEmail = await createEmailTemplate({
+        slug: 'a-1',
+        template: 'hello'
+      })
       assert.doesNotThrow(async () => await deleteEmailTemplate(existingEmail.id))
-    })
-  })()
+    })()
+  })
 })
 
 describe('testing updateEmailTemplate service', function () {
   before(beforeFunction)
 
-  return (async () => {
-    const existingEmail = await createEmailTemplate({
-      slug: 'a-1',
-      template: 'hello'
-    })
-    it('should not throw error', async () => {
+  it('should not throw error', async () => {
+    return (async () => {
+      const existingEmail = await createEmailTemplate({
+        slug: 'a-1',
+        template: 'hello'
+      })
       assert.doesNotThrow(async () => await updateEmailTemplate(
         existingEmail.id,
         {
           slug: 'a-2'
         }
       ))
-    })
+    })()
+  })
 
-    it('should throw error', async () => {
+  it('should throw error', async () => {
+    return (async () => {
+      const existingEmail = await createEmailTemplate({
+        slug: 'a-2',
+        template: 'hello'
+      })
       await createEmailTemplate({
         slug: 'a-3',
         template: 'hello'
@@ -133,6 +140,6 @@ describe('testing updateEmailTemplate service', function () {
         ),
         error as Error
       )
-    })
-  })()
+    })()
+  })
 })
