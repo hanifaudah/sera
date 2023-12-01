7dc9ab5acd8cf94e2b6f52e818567e28112a0f03
hanifaudah <hanifarkanaudah@gmail.com>
2023-12-01 22:10:13 +0700

    test(api): setup testing

---
+++ api/tests/unit/emailTemplateCrudService.test.ts
@@ -0,0 +1,11 @@
+import assert from 'node:assert'
+
+describe('the tests are working', function () {
+  it('should pass', () => {
+    assert.equal(1 + 2, 3)
+  })
+})
+
+describe('testing listEmailTemplates service', function () {
+  it('should pass')
+})
